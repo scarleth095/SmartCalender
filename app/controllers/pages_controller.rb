0aa36d3d348518affb2b469dd4d68b79bd23307b
Scarleth Estevez <scarleth@bu.edu>
2018-02-12 21:30:45 -0500

    heroic set up

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,15 @@
 class PagesController < ApplicationController
   
-
+require 'date'
+  
 def calendar
       @event = Event.new
     @events=Event.where("date > ?", 0) 
+    d = Date.today
+
+    from=d.change(:day => 1)
+    
+    @holidays=Holidays.between(from, d.end_of_month, :us)
      
 
 end
